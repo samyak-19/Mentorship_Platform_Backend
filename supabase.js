aee5f4a894d143d05497b8a85dbb4be29cfd8ebe
samyak-19 <samlinkin19@gmail.com>
2026-04-03 13:54:21 +0530

    backend deployment

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
